69ae67d171232ffb1f4d63635c5cabd5d90cf993
RRP12 <rushikesh.patil@brainvire.com>
2024-11-13 15:30:42 +0530

    next js setup complete

---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,73 @@
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import { GiSelfLove } from "react-icons/gi";
+import NavLink from "./NavLink";
+
+export default async function TopNav() {
+  // const session = await auth();
+  // const userInfo = session?.user && (await getUserInfoForNav());
+
+  const memberLinks = [
+    { href: "/members", label: "Matches" },
+    { href: "/lists", label: "Lists" },
+    { href: "/messages", label: "Messages" },
+  ];
+
+  const adminLinks = [
+    {
+      href: "/admin/moderation",
+      label: "Photo Moderation",
+    },
+  ];
+
+  // const links = session?.user.role === "ADMIN" ? adminLinks : memberLinks;
+  return (
+    <>
+      <Navbar
+        maxWidth="full"
+        className="bg-gradient-to-r from-pink-400 via-red-400 to-pink-600"
+        classNames={{
+          item: [
+            "text-xl",
+            "text-white",
+            "uppercase",
+            "data-[active=true]:text-yellow-200",
+          ],
+        }}
+      >
+        <NavbarBrand as={Link} href="/">
+          <GiSelfLove size={40} className="text-gray-200" />
+          <div className="font-bold text-3xl flex">
+            <span className="text-gray-200">MatchMe</span>
+          </div>
+        </NavbarBrand>
+        <NavbarContent justify="center">
+          {true &&
+            memberLinks.map((item) => (
+              <NavLink key={item.href} href={item.href} label={item.label} />
+            ))}
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <Button
+            as={Link}
+            href="/login"
+            variant="bordered"
+            className="text-white"
+          >
+            Login
+          </Button>
+          <Button
+            as={Link}
+            href="/register"
+            variant="bordered"
+            className="text-white"
+          >
+            Register
+          </Button>
+        </NavbarContent>
+      </Navbar>
+      {/* <FiltersWrapper /> */}
+    </>
+  );
+}
---
+++ src/messages/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Meassagespage() {
+  return <div>measgaespage</div>;
+}
